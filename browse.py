4229cd4b5d7783d83c0e3259f25d6b6f01044197
Jonathan <jonforscher@gmail.com>
2014-02-16 12:45:40 -0500

    Printing out errors and properly getting subreddit info.

--- browse.py
+++ browse.py
@@ -16,58 +16,39 @@ def login(username, password):
     client = requests.session()
     client.headers=headers
     r = client.post('http://www.reddit.com/api/login', data=UP)
+
+    # Print out error codes on attempting login
+    j = json.loads(r._content)
+    for jsons, userData in j.iteritems():
+        print userData.pop(u'errors')
+        break
+
     client.user = username
     def name():
         return '{}\'s client'.format(username)
 
+    #grabs the modhash from the response
+    client.modhash = j['json']['data']['modhash']
+
     return client
- 
-#----------------------------------------------------------------------
-def subredditInfo(client, limit, sr='askreddit',
-                  sorting='', return_json=False, **kwargs):
-    """retrieves X (max 100) amount of stories in a subreddit
-    'sorting' is whether or not the sorting of the reddit should be customized or not,
-    if it is: Allowed passing params/queries such as t=hour, week, month, year or all"""
- 
-    #query to send
-    parameters = {'limit': limit,}
-    parameters.update(kwargs)
- 
-    url = r'http://www.reddit.com/r/{sr}/{top}.json'.format(sr=sr, top=sorting)
-    r = client.get(url,params=parameters)
-    print 'sent URL is', r.url
-    j = json.loads(r.text)
- 
-    #return raw json
-    if return_json:
-        return j
- 
-    #or list of stories
-    else:
-        stories = []
-        for story in j['data']['children']:
-            stories.append(story['data']['title'])
- 
-        return stories
 
-
-def userSubreddit(client, limit, return_json=False, **kwargs):
+def usersubreddit(client, limit, return_json=False, **kwargs):
 
     parameters = {'limit': limit,}
     parameters.update(kwargs)
     url = r'http://www.reddit.com/subreddits/mine/subscriber.json'
-    r = client.get(url, params=parameters)
-    print r
-    # j = json.loads(r.text)
+    r = client.get(url)
+    j = json.loads(r.text)
 
-    # if return_json:
-    #     return j
-    # else:
-    #     subreddits = []
-    #     for subreddit in j['data']['children']:
-    #         subreddits.append(subreddit['data']['permalink'])
-
-    #     return subreddits
+    if return_json:
+        return j
+    else:
+        stories = []
+        for story in j['data']['children']:
+            #print story['data']['title']
+            stories.append(story)
+        return stories
+        
 
 
 def main():
@@ -75,7 +56,7 @@ def main():
     username = raw_input("-->")
     password = getpass.getpass()
     client = login(username, password)
-    j = userSubreddit(client, limit=10)
+    j = usersubreddit(client, limit=10)
     pprint(j)
 if  __name__ =='__main__':main()
 
